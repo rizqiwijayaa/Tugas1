6047a49225de93858fac967ba82da0b5992c541f
Rizqi Wijaya <rizqiwijaya4@gmail.com>
2025-02-27 10:55:53 +0700

    Tugas 1

---
+++ Array1-6.java
@@ -0,0 +1,66 @@
+public class latihan {
+    public static void main(String[] args) {
+        // deklrasi dan inisialisasi
+        int[] number = new int[5];
+        // inisialisasi
+        number[0]=1;
+        number[1]=2;
+        number[2]=3;
+        number[3]=4;
+        number[4]=5;
+
+        // soal1
+        // for (int i = 0; i < number.length; i++) {
+        //         System.out.print(number[i]+" ");     
+        // }
+
+
+        // soal2
+        // for (int i = 0; i < number.length; i++) {
+        //         System.out.print(number[i]);  
+        //         if (i<4) {
+        //                 System.out.print(",");
+        //     }     
+        // }
+
+        // soal3
+        // for (int i = 0; i < number.length; i++) {
+        //     if (i%2==0) {
+        //         System.out.print(number[i]);  
+        //         if (i<4) {
+        //                 System.out.print(",");
+        //             } 
+        //     }     
+        // }
+
+        // soal4
+        // for (int i = 4; i > -1; i--) {
+        //     if (i%2==0) {
+        //         System.out.print(+number[i]); 
+        //         if (i>1) {
+        //             System.out.print(",");
+        //         }
+        //     } 
+        // }
+
+        // soal5
+        // for (int i = 0; i < number.length; i++) {
+        //     if (i%2==1) {
+        //         System.out.print(+number[i]);  
+        //         if (i<2) {
+        //             System.out.print(",");
+        //         } 
+        //     }     
+        // }
+
+        // soal6
+        // int[] pola = {0, 0, 2, 1, 4}; // Pola indeks untuk menghasilkan output 1,1,3,2,5
+        // for (int i = 0; i < pola.length; i++) {
+        // System.out.print(number[pola[i]]);
+        // if (i < pola.length - 1) {
+        // System.out.print(",");
+        // }
+        // }
+        
+    }
+}
